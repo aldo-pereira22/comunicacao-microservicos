5135992d554e5d13369190740bc9246f40ef32fb
Aldo <aldopereira.gyn@gmail.com>
2023-02-22 23:06:47 -0300

    Criação do AuthDB

---
+++ auth-api/app.js
@@ -0,0 +1,18 @@
+import express from "express";
+
+const app = express();
+
+const env = process.env;
+const PORT = env.port || 8080;
+
+
+app.get("/api/status",(req, res) => {
+    return res.status(200).json({
+        service: "Auth-API",
+        status: "UP",
+        httpStatus: 200
+    })
+})
+app.listen(PORT, ()=> {
+    console.info("Server started sucessfull at port: ", PORT);
+})

e6ae0f02941aee411174598ea8d852945c122041
Aldo <aldopereira.gyn@gmail.com>
2023-03-04 23:03:32 -0300

    Interceptador de Token

--- product-API/src/main/java/br/com/products/api/interception/AuthInterceptor.java
+++ product-API/src/main/java/br/com/products/api/interception/AuthInterceptor.java
@@ -1,6 +1,8 @@
 package br.com.products.api.interception;
 
+import br.com.products.api.modules.jwt.service.JwtService;
 import feign.Request;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 import javax.servlet.http.HttpServletRequest;
@@ -9,15 +11,21 @@ import javax.servlet.http.HttpServletResponse;
 public class AuthInterceptor implements HandlerInterceptor {
 
     private static final String AUTHORIZATION = "Authorization";
+
+    @Autowired
+    private JwtService jwtService;
+
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler){
         if(isOptions(request)){
             return true;
         }
-        var authorization = request.getHeader();
-        return false;
+        var authorization = request.getHeader(AUTHORIZATION);
+
+        jwtService.validateAuthorization(authorization);
+        return true;
     }
 
     private boolean isOptions(HttpServletRequest request){
-        return Request.HttpMethod.OPTIONS.name().equals(request.getMethod())
+        return Request.HttpMethod.OPTIONS.name().equals(request.getMethod());
     }
 }

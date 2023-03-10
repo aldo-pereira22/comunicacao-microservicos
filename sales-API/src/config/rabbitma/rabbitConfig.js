45c5daff6ac2e7d0b5eb08539b15332126d05a41
Aldo <aldopereira.gyn@gmail.com>
2023-03-10 14:33:07 -0300

    Configuração do RabbitMQ

--- sales-API/src/config/rabbitma/rabbitConfig.js
+++ sales-API/src/config/rabbitma/rabbitConfig.js
@@ -10,19 +10,36 @@ import {
 
 
 const HALF_SECOND = 500
+const HALF_MINUTE = 30000
+const CONTAINER_ENV = "container"
+
 
 export async function connectRabbitMq(){
-    amqp.connect(RABBIT_MQ_URL, (error, connection) => {
+  const env = process.env.NODE_ENV
+
+  if(CONTAINER_ENV === env){
+    console.info("Waiting for rabittMQ to start...")
+    setInterval(() => {
+      conectRabittMqAndCreateQueues()
+    }, HALF_MINUTE)
+  }else {
+    await conectRabittMqAndCreateQueues()
+  }
+}
+    async function conectRabittMqAndCreateQueues(){
+            amqp.connect(RABBIT_MQ_URL, (error, connection) => {
         if(error){
             throw new error
         }
+        console.info("Starting RABBIT MQ ...")
         createQueue(connection, PRODUCT_STOCK_UPDATE_QUEUE, PRODUCT_STOCK_UPDATE_ROUTING_KEY, PRODUCT_TOPIC)
         createQueue(connection, SALES_CONFIRMATION_QUEUE, SALES_CONFIRMATION_ROUTING_KEY, PRODUCT_TOPIC)
-
+        console.info("Queues and Topics were defined ...")
         setTimeout(() => {
             connection.close()
         }, HALF_SECOND)
-    })    
+    })  
+    }
 
     function createQueue(connection, queue, routingKey, topic){
         connection.createChannel((error, channel) => {
@@ -37,4 +54,4 @@ export async function connectRabbitMq(){
     }
 
     
-}
+
